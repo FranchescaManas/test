3aa9e2ec89bff4a34cef4f4316ca20d33913fc91
Franchesca De Guzman <95163739+FranchescaManas@users.noreply.github.com>
2024-09-29 15:30:11 +0800

    initial commit

---
+++ views/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+</head>
+<body class="bg-light">
+    <div style="height: 100vh;">
+        <div class="row h-100 m-0">
+            <div class="card w-25 my-auto mx-auto">
+                <div class="card-header bg-white border-0 py-3">
+                    <h1 class="text-center">REGISTER</h1>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
